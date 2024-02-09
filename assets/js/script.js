898699df4a04346abe335a0ee30dcdd6ca3831f1
ossama Aldawamenah <ossamaomar@gmail.com>
2024-02-09 11:39:42 +0000

    reading the category and difficulty level to creat the API url

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,18 @@
 let dataBase = [];
 let correctlyAnswered = [];
 let wronglyAnswered = [];
+let start=0
+// let decr=-1;
 
-// Putting the URL Name for the API together 
+
+// Define the URL for the API
 let urlName = "";
 let catagoryButtonValue = '23';
-let dificultyButtonValue = 'medium'
+let dificultyButtonValue = 'medium';
 urlName = `https://opentdb.com/api.php?amount=10&category=${catagoryButtonValue}&difficulty=${dificultyButtonValue}&type=multiple`;
-console.log(urlName);
 
 // Fetch data from the URL
-// fetch('https://opentdb.com/api.php?amount=1&category=23&difficulty=medium&type=multiple')
-fetch(`${urlName}`)
+fetch(urlName)
     .then(response => {
         // Check if the response is successful
         if (!response.ok) {
@@ -22,44 +23,56 @@ fetch(`${urlName}`)
     })
     .then(data => {
         // Work with the JSON data
-        console.log(data);
-
         dataBase = data.results;
-        console.log(dataBase);
+        // Call a function to handle the fetched data
+        handleFetchedData();
+    })
+    .catch(error => {
+        // Handle errors
+        console.error('There was a problem with the fetch operation:', error);
+    });
 
-        let item = 0;
-        console.log(item);
-        console.log(dataBase[item].question);
-        document.getElementById('userQuestion').innerText = dataBase[0].question;
-        console.log("Correct Answer: ", dataBase[item].correct_answer);
-        document.getElementById('firstAns').innerText = dataBase[0].correct_answer;
-        document.getElementById('flexRadioDefault1').value = dataBase[0].correct_answer;
-        console.log(document.getElementById('flexRadioDefault1').value);
-        console.log('fjdsklfjdskl');
-        document.getElementById('2ndAns').innerText = dataBase[0].incorrect_answers[0];
-        document.getElementById('3rdAns').innerText = dataBase[0].incorrect_answers[1];
-        document.getElementById('4thAns').innerText = dataBase[0].incorrect_answers[2];
-        console.log("Incorrect Answers: ", dataBase[item].incorrect_answers.join(", "));
-        console.log("---------------------------");
-        var userAnswer = dataBase[item].correct_answer;
+// Function to handle the fetched data
+function handleFetchedData() {
+    
+    function handleClick(radioButton) {
+    
+    let item = 0;
+    console.log(item);
+    console.log(dataBase[item].question);
+    document.getElementById('userQuestion').innerText = dataBase[0].question;
+    console.log("Correct Answer: ", dataBase[item].correct_answer);
+    document.getElementById('firstAns').innerText = dataBase[0].correct_answer;
+    document.getElementById('flexRadioDefault1').value = dataBase[0].correct_answer;
+    console.log(document.getElementById('flexRadioDefault1').value);
+    console.log('fjdsklfjdskl');
+    document.getElementById('2ndAns').innerText = dataBase[0].incorrect_answers[0];
+    document.getElementById('3rdAns').innerText = dataBase[0].incorrect_answers[1];
+    document.getElementById('4thAns').innerText = dataBase[0].incorrect_answers[2];
+    console.log("Incorrect Answers: ", dataBase[item].incorrect_answers.join(", "));
+    console.log("---------------------------");
+    var userAnswer = dataBase[item].correct_answer;
+    
+   
 
-        function handleClick(radioButton) {
-            // Access the value of the clicked radio button
-            let radioButtons = document.getElementsByName('flexRadioDefault');
+    // Access the value of the clicked radio button
+        let radioButtons = document.getElementsByName('flexRadioDefault');
 
-            // Iterate through all radio buttons to find the selected one
-            let selectedValue;
-            for (let i = 0; i < radioButtons.length; i++) {
-                if (radioButtons[i].checked) {
-                    selectedValue = radioButtons[i].value;
-                    break; // Stop iterating once a selected radio button is found
-                }
+        // Iterate through all radio buttons to find the selected one
+        let selectedValue;
+        for (let i = 0; i < radioButtons.length; i++) {
+            if (radioButtons[i].checked) {
+                selectedValue = radioButtons[i].value;
+                break; // Stop iterating once a selected radio button is found
             }
+        }
 
-            // Now you have the selected value
-            console.log('Selected value:', selectedValue);
-            // const selectedValue = radioButton.value;
-            if (selectedValue === userAnswer) { // Use === for comparison, not =
+        // Now you have the selected value
+        console.log('Selected value:', selectedValue);
+        // const selectedValue = radioButton.value;
+        if (selectedValue === userAnswer) { // Use === for comparison, not =
+            
+            if (start !== 0){
                 console.log('WOWOWO');
                 let incr = parseInt(document.getElementById('correctAnsCount').innerText);
                 console.log(incr);
@@ -70,42 +83,40 @@ fetch(`${urlName}`)
                 correctlyAnswered.push(dataBase[0]);
                 console.log(correctlyAnswered);
 
-                // let indexToRemove = dataBase.indexOf("this");
-
                 // If the element is found, remove it
-                // if (indexToRemove !== -1) {
                 dataBase.splice(0, 1);
-                // }
-
                 console.log(dataBase);
-            } else {
+            }
+        } else {
+            if (start !== 0){
+
                 console.log('NoNoNo');
                 let decr = parseInt(document.getElementById('wrongAnsCount').innerText);
                 console.log(decr);
                 decr++;
                 document.getElementById('wrongAnsCount').innerText = decr;
+                
                 console.log(decr);
                 // Add the question to the correctly answered questions Array.
                 wronglyAnswered.push(dataBase[0]);
                 console.log(wronglyAnswered);
 
-                // let indexToRemove = dataBase.indexOf("this");
                 // If the element is found, remove it
-                // if (indexToRemove !== -1) {
                 dataBase.splice(0, 1);
                 console.log(dataBase);
             }
-            // Perform actions based on the selected value
-            console.log("Selected value:", selectedValue);
-            // Add more code to handle the click event as needed
         }
+        // Perform actions based on the selected value
+        console.log("Selected value:", selectedValue);
+        // Add more code to handle the click event as needed
+        start =1;
+    }
 
-        // Attach the handleClick function to the click event of radio buttons
-        document.getElementById('next').addEventListener('click', function () {
-            handleClick(this); // 'this' refers to the clicked radio button
-        });
-    })
-    .catch(error => {
-        // Handle errors
-        console.error('There was a problem with the fetch operation:', error);
+    // Attach the handleClick function to the click event of the "Next" button
+    document.getElementById('next').addEventListener('click', function () {
+        handleClick(this); // 'this' refers to the clicked button
     });
+}
+
+// Attach the handleClick function to the click event of the "Next" button
+ 
